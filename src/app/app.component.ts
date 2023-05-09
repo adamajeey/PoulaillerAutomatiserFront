a5afb8c9c5da6cb49358c47e57e25e2f6f7027c1
LAYE MAKHTAAR <adama jeey>
2023-05-09 12:04:38 +0000

    reglage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PoulaillerFront';
+  showLogin: boolean = false;
+  localStatus = localStorage.getItem('currentUser');
+  constructor(private auth: UsersService, private router: Router, private http: HttpClient) {
+
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        if (event.url === '/login' || event.url === '/') {
+          this.showLogin = true;
+        } else {
+          this.showLogin = false;
+          if(!this.localStatus){
+            window.location.pathname=''
+          }
+        }
+      }
+    });
+  }
+
 }
